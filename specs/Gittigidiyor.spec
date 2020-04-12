51e6d69d857420b5d04e7d11af3902a87e87ce6f
CanOklay <canoklay@gmail.com>
2020-04-12 23:37:33 +0300

    screenshot

--- specs/Gittigidiyor.spec
+++ specs/Gittigidiyor.spec
@@ -9,4 +9,11 @@ Get Started
 -----------
 * Arama alanına tıklanır
 * Arama kısmına "telefon" yazılır
-* Vazgeç butonuna tıklanır
+* Ekran görüntüsü al ve "1" ismiyle kaydet
+* Açılan sekmeye tıklanır
+* Açılan ürünlerden ilki seçilir
+* Ekran görüntüsü al ve "2" ismiyle kaydet
+* Üründen "1" adet ekle
+* Sepete ekle butonuna tıklanır
+* Sepetim sekmesine gidilir
+* Ekran görüntüsü al ve "3" ismiyle kaydet
--- src/test/java/com/testinium/gittigidiyor/StepImplementation.java
+++ src/test/java/com/testinium/gittigidiyor/StepImplementation.java
@@ -1,8 +1,15 @@
 package com.testinium.gittigidiyor;
 
+import com.thoughtworks.gauge.Gauge;
 import com.thoughtworks.gauge.Step;
 import driver.Driver;
 import io.appium.java_client.MobileElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
 
 public class StepImplementation extends Driver {
 
@@ -19,9 +26,52 @@ public class StepImplementation extends Driver {
         elementTwo.sendKeys(key);
      }
 
-     @Step("Vazgeç butonuna tıklanır")
-    public void clickCancel() {
-        MobileElement elementThree = (MobileElement) appiumDriver.findElementById("cancelButton");
+     @Step("Açılan sekmeye tıklanır")
+    public void clickOne() {
+        MobileElement elementThree = (MobileElement) appiumDriver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[1]/android.widget.RelativeLayout/android.widget.TextView\n");
         elementThree.click();
      }
+
+     @Step("Açılan ürünlerden ilki seçilir")
+    public void pickProduct() {
+        MobileElement elementFour = (MobileElement) appiumDriver.findElementByXPath("/hierarchy/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.FrameLayout[1]/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.RelativeLayout");
+        elementFour.click();
+    }
+
+
+    @Step("Sepete ekle butonuna tıklanır")
+    public void addBasket() {
+        MobileElement elementFive = (MobileElement) appiumDriver.findElementById("btnAddBasket");
+        elementFive.click();
+    }
+
+    @Step("Sepetim sekmesine gidilir")
+    public void basketPage() {
+        MobileElement elementSix = (MobileElement) appiumDriver.findElementByXPath("/hierarchy/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.RelativeLayout[3]");
+        elementSix.click();
+    }
+
+    @Step("Üründen <key> adet ekle")
+    public void increaseProduct(String key) {
+        if (key.equals("iki")) {
+            MobileElement increaseProduct = (MobileElement) appiumDriver.findElementById("btnPlusVariant");
+            increaseProduct.click();
+        } else if (key.equals("üç")) {
+            for (int i = 0; i < 2; i++) {
+                MobileElement increaseProduct = (MobileElement) appiumDriver.findElementById("btnPlusVariant");
+                increaseProduct.click();
+            }
+        }
+    }
+
+    @Step("Ekran görüntüsü al ve <picturename> ismiyle kaydet")
+    public void takesScreenshot(String pictureName) throws IOException {
+        String filename = "images/" + (pictureName) + ".png";
+        File file = new File("reports/gtml-report/" + filename);
+        if (file.exists())
+            file.delete();
+        File scrFile = ((TakesScreenshot)appiumDriver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, file);
+        Gauge.writeMessage("<img src='../" + filename + "' width = '800' height = '480'");
+    }
 }
