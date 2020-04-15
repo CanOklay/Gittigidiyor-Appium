fbf7d3edcc2e9ce85d1e396392bcaae9cbe0ecf8
CanOklay <canoklay@gmail.com>
2020-04-15 17:21:20 +0300

    deneme

--- specs/Gittigidiyor.spec
+++ specs/Gittigidiyor.spec
@@ -9,11 +9,9 @@ Get Started
 -----------
 * Arama alanına tıklanır
 * Arama kısmına "telefon" yazılır
-* Ekran görüntüsü al ve "1" ismiyle kaydet
+* Kayda başla ve bitir
 * Açılan sekmeye tıklanır
 * Açılan ürünlerden ilki seçilir
-* Ekran görüntüsü al ve "2" ismiyle kaydet
 * Üründen "1" adet ekle
 * Sepete ekle butonuna tıklanır
 * Sepetim sekmesine gidilir
-* Ekran görüntüsü al ve "3" ismiyle kaydet
--- src/test/java/com/testinium/gittigidiyor/StepImplementation.java
+++ src/test/java/com/testinium/gittigidiyor/StepImplementation.java
@@ -1,5 +1,7 @@
 package com.testinium.gittigidiyor;
 
+import atu.testrecorder.ATUTestRecorder;
+import atu.testrecorder.exceptions.ATUTestRecorderException;
 import com.thoughtworks.gauge.Gauge;
 import com.thoughtworks.gauge.Step;
 import driver.Driver;
@@ -7,12 +9,23 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.touch.offset.PointOption;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.interactions.touch.TouchActions;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
+import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class StepImplementation extends Driver {
 
@@ -79,10 +92,48 @@ public class StepImplementation extends Driver {
         Gauge.writeMessage("<img src='../" + filename + "' width = '800' height = '480'");
     }
 
+    /*@Step("<key> email adresine mail gönderilir")
+    public void sendEmail(String key) throws EmailException, MessagingException {
+        System.out.println("----------E-mail gönderme kısmı başladı----------");
+        Email email = new SimpleEmail();
+        email.setHostName("smtp.gmail.com");
+        email.setSmtpPort(465);
+        email.setAuthenticator(new DefaultAuthenticator("canoklay@gmail.com", "642378111fco"));
+        email.setSSLOnConnect(true);
+        email.setFrom("canoklay@gmail.com");
+        email.setSubject("Gittigidiyor Test Raporu");
+        email.setMsg("Bu mail otomatik olarak Selenium tarafından gönderilmiştir.");
+        String filename = "reports/html-report/images/1.png";
+        // Create another object to add another content
+        MimeBodyPart messageBodyPart2 = new MimeBodyPart();
+
+        // Create data source and pass the filename
+        DataSource source = (DataSource) new FileDataSource(filename);
+
+        // set the handler
+        messageBodyPart2.setDataHandler(new DataHandler((javax.activation.DataSource) source));
+
+        // set the file
+        messageBodyPart2.setFileName(filename);
+        email.addTo(key);
+        //key kısmı parametre olarak verildi. Spec'te çağırırken göndereceğimiz E-mail'i yazacağız.
+        email.send();
+        System.out.println("----------E-mail gönderildi----------");
+    }*/
+
+    @Step("Kayda başla ve bitir")
+    public void record() throws ATUTestRecorderException, InterruptedException {
+        DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd-HH-mm-ss");
+        Date date = new Date();
+        ATUTestRecorder recorder = new ATUTestRecorder("/Users/canoklay/Desktop", "SenaryoKaydı-" + dateFormat.format(date), false);
+        recorder.start();
+        System.out.println("Kayda başla");
+        Thread.sleep(4000);
+        recorder.stop();
+    }
+
     public void scroll(int startx, int starty, int endx, int endy) {
-
         TouchAction touchAction = new TouchAction(appiumDriver);
-
         touchAction.longPress(PointOption.point(startx, starty))
                 .moveTo(PointOption.point(endx, endy))
                 .release()
