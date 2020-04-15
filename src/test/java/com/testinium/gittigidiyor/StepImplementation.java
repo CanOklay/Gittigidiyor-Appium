266dd456086bec90bc459ee80a20b4c24c34f824
CanOklay <canoklay@gmail.com>
2020-04-15 23:21:17 +0300

    screen

--- src/test/java/com/testinium/gittigidiyor/StepImplementation.java
+++ src/test/java/com/testinium/gittigidiyor/StepImplementation.java
@@ -90,7 +90,7 @@ public class StepImplementation extends Driver {
         File scrFile = ((TakesScreenshot)appiumDriver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(scrFile, file);
         Gauge.writeMessage("<img src='../" + filename + "' width = '800' height = '480'");
-        System.out.println("Screenshot alındı");
+        System.out.println("Screenshot alı");
     }
 
     /*@Step("<key> email adresine mail gönderilir")
